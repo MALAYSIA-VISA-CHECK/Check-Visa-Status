a9bf079d9b4f62d6b504f091199963418124ae7d
MALAYSIA-VISA-CHECK <tasirahamed350@gmail.com>
2025-01-14 20:02:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function searchDocument() {
+    var documentNo = document.getElementById("documentNo").value;
+    if (documentNo === "AB123456") {
+        document.getElementById("resultTable").getElementsByTagName("tbody")[0].innerHTML = `
+            <tr>
+                <td>1</td>
+                <td>SUBEL KHAN</td>
+                <td>BANGLADESH</td>
+                <td>A15407050</td>
+                <td></td>
+                <td>VISIT PASS (SOCIAL)</td>
+                <td></td>
+                <td></td>
+                <td>SINGLE ENTRY VISA</td>
+                <td>PRINT</td>
+            </tr>
+        `;
+        document.getElementById("totalRecord").innerText = "Total Record: 1";
+    } else {
+        document.getElementById("warningMessage").style.display = "block";
+        document.getElementById("resultTable").getElementsByTagName("tbody")[0].innerHTML = `
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        `;
+        document.getElementById("totalRecord").innerText = "Total Record: 0";
+    }
+}
+
+function resetForm() {
+    // Reset form fields to default values
+    document.getElementById("documentNo").value = "";
+    document.getElementById("countryIssue").value = "BANGLADESH";
+    document.getElementById("documentduedate").value = "";
+    document.getElementById("applicationNo").value = "";
+    document.getElementById("transactionType").value = "LANJUTAN";
+    document.getElementById("applicationDate").value = "";
+
+    // Reset table content and total record
+    document.getElementById("resultTable").getElementsByTagName("tbody")[0].innerHTML = `
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    `;
+    document.getElementById("totalRecord").innerText = "Total Record: 0";
+
+    // Hide warning message
+    document.getElementById("warningMessage").style.display = "none";
+}
